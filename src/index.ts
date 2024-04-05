e2d2ac7468e18ba669141211612b072aaa8250a8
Nitesh Shetye <niteshshetye204@gmail.com>
2024-04-05 20:05:41 +0530

    blog CRUD done

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,54 @@
 import { Hono } from "hono";
 import { auth } from "./routes/auth";
 import { blog } from "./routes/blog";
+import { logger } from "hono/logger";
+import { poweredBy } from "hono/powered-by";
+import { verify } from "hono/jwt";
 
-const app = new Hono().basePath("/api/v1");
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    MY_JWT_SECRET: string;
+  };
+  Variables: {
+    userid: string;
+  };
+}>().basePath("/api/v1");
+
+app.use(poweredBy());
+app.use(logger());
+
+/**
+ *  extract token from heading
+ *  verify token
+ *  if token is valid than next()
+ *  else give error if token is in valid
+ */
+
+app.use("/blog/*", async (c, next) => {
+  const authorization = c.req.header("authorization") || "";
+  const token = authorization.split(" ")[1];
+
+  if (!token) {
+    c.status(403);
+    return c.json({ message: "Unauthorized" });
+  }
+
+  try {
+    const payload = await verify(token, c.env.MY_JWT_SECRET);
+
+    if (payload.id) {
+      c.set("userid", payload.id);
+      await next();
+    } else {
+      c.status(403);
+      return c.json({ message: "Unauthorized" });
+    }
+  } catch (error) {
+    c.status(500);
+    return c.json(error);
+  }
+});
 
 app.route("/auth", auth);
 app.route("/blog", blog);
--- src/routes/blog.ts
+++ src/routes/blog.ts
@@ -1,19 +1,116 @@
 import { Hono } from "hono";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaClient } from "@prisma/client/edge";
 
-const blog = new Hono();
+type CreatPost = {
+  title: string;
+  content: string;
+  authorId: string;
+};
+
+const blog = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    MY_JWT_SECRET: string;
+  };
+  Variables: {
+    userid: string;
+  };
+}>();
 
 blog
-  .post("/", (c) => {
-    return c.text("add blog");
+  .post("/", async (c) => {
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    try {
+      const body = await c.req.json<CreatPost>();
+
+      const posts = await prisma.post.create({
+        data: { ...body, authorId: c.get("userid") },
+      });
+
+      c.status(201);
+      return c.json(posts);
+    } catch (error) {
+      c.status(404);
+      return c.json(error);
+    }
   })
-  .put("/:id", (c) => {
-    return c.text("update blog");
+  .put("/:id", async (c) => {
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const { id } = c.req.param();
+
+    try {
+      const body = await c.req.json();
+
+      console.log(body);
+
+      if (!body) {
+        c.status(404);
+        return c.json({ message: "Invalid input" });
+      }
+
+      const post = await prisma.post.update({
+        where: {
+          id,
+        },
+        data: body,
+      });
+      if (!post) {
+        c.status(404);
+        return c.json({ message: "Post not found!" });
+      }
+
+      c.status(200);
+      return c.json(post);
+    } catch (error) {
+      c.status(404);
+      return c.json(error);
+    }
   })
-  .get("/:id", (c) => {
-    return c.text("get blog");
+  .get("/:id", async (c) => {
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    const { id } = c.req.param();
+
+    try {
+      const post = await prisma.post.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      c.status(201);
+      return c.json(post);
+    } catch (error) {
+      c.status(404);
+      return c.json(error);
+    }
   })
-  .get("/", (c) => {
-    return c.text("get all blog");
+  .get("/", async (c) => {
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+    }).$extends(withAccelerate());
+
+    try {
+      const blogs = await prisma.post.findMany({
+        orderBy: {
+          modifiedAt: "desc",
+        },
+      });
+      c.status(200);
+      return c.json(blogs);
+    } catch (error) {
+      c.status(404);
+      return c.json(error);
+    }
   });
 
 export { blog };
